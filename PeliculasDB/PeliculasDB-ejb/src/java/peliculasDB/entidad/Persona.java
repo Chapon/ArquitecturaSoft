d6c8641606715f35d2fd9ffe7426deada98bba9e
URXS814 <URXS814@LMVD1005.TAG.local>
2015-06-09 20:46:04 -0300

    Actualizacion de entidades

--- PeliculasDB/PeliculasDB-ejb/src/java/peliculasDB/entidad/Persona.java
+++ PeliculasDB/PeliculasDB-ejb/src/java/peliculasDB/entidad/Persona.java
@@ -8,7 +8,6 @@ public class Persona extends EntidadPersistente {
     
     @Column(nullable = false)
     private String nombre;
-    
     @Column(nullable = false)
     private String email;
     
@@ -28,4 +27,30 @@ public class Persona extends EntidadPersistente {
         this.email = e;
     }
     
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", email=" + email + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (this.nombre != null ? this.nombre.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Persona other = (Persona) obj;
+        if ((this.nombre == null) ? (other.nombre != null) : !this.nombre.equals(other.nombre)) {
+            return false;
+        }
+        return true;
+    }
 }
--- PeliculasDB/PeliculasDB-ejb/src/java/peliculasDB/entidad/Resenia.java
+++ PeliculasDB/PeliculasDB-ejb/src/java/peliculasDB/entidad/Resenia.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package peliculasDB.entidad;
 
-/**
- *
- * @author URXS814
- */
-class Resenia {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Resenia extends EntidadPersistente{
     
+    @Column(nullable = false)
+    @OneToOne (mappedBy = "Resenia")
+    private Pelicula pelicula;
+    @Column(nullable = false)
+    private String nota;
+    
+    @Column(nullable = false)
+    private int calificacion;
+    
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+    
+    public void setPelicula(Pelicula p) {
+        this.pelicula = p;
+    } 
+    
+    public String getNota() {
+        return nota;
+    }
+    
+    public void setNota(String n) {
+        this.nota = n;
+    }    
+    
+    public int getCalificacion() {
+        return calificacion;
+    }
+    
+    public void setCalificacion(int c) {
+        this.calificacion = c;
+    }  
+    
+    @Override
+    public String toString() {
+        return "Resenia{" + "pelicula=" + this.pelicula + ", nota=" + this.nota + ", calificacion=" + this.calificacion +'}';
+    }
+
+    /*@Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (this.getPelicula() != null ? this.getPelicula().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+        final Resenia other = (Resenia) object;
+        if ((this.getPelicula() == null) ? (other.getPelicula() != null) : !this.getPelicula().equals(other.getPelicula())) {
+            return false;
+        }
+        return true;
+    }*/ 
 }
